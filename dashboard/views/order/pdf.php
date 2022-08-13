79c942f06022d808c6f74d3938343c6cc1113629
mohammed_alabadi <m.almahameed@syarah.com>
2022-08-13 20:48:47 +0300

    master

--- dashboard/views/order/pdf.php
+++ dashboard/views/order/pdf.php
@@ -1,13 +1,19 @@
 <?php
 /* @var $this yii\web\View */
+
 /* @var $model \common\models\InventoryOrder */
+
 use yii\helpers\Html;
+use common\components\Arabic;
+
+
+$Arabic = new  Arabic();
 ?>
 
 <table border="0" width="100%" style="padding-bottom: 20px">
     <tr>
         <td align="center">
-           وجدي للاعمار ومواد البناء
+            وجدي للاعمار ومواد البناء
         </td>
     </tr>
 </table>
@@ -15,17 +21,17 @@ use yii\helpers\Html;
 <table border="0" width="100%" dir="rtl">
     <tr>
         <td bgcolor="#eee" width="33.3%">
-            رقم الفاتورة : <?= $model->id;?><br>
-            اسم المشتري : <?= $model->customerTitle;?><br>
-            رقم المشتري : <?= $model->customer->phone_number ?? '';?><br>
+            رقم الفاتورة : <?= $model->id; ?><br>
+            اسم المشتري : <?= $model->customerTitle; ?><br>
+            رقم المشتري : <?= $model->customer->phone_number ?? ''; ?><br>
 
         </td>
         <td align="center" width="33.3%">
-           <?php echo Html::img('@web/images/site/icons/vip.svg'); ?>
+            <?php echo Html::img('@web/images/site/icons/vip.svg'); ?>
         </td>
         <td bgcolor="#eee" width="33.3%">
-            اسم مصدر الفاتورة  : <?= $model->getUserName('created_by');?><br>
-            تاريخ الاصدار : <?= $model->getDate('created_at');?><br>
+            اسم مصدر الفاتورة : <?= $model->getUserName('created_by'); ?><br>
+            تاريخ الاصدار : <?= $model->getDate('created_at'); ?><br>
             اسم المحل : <?= $model->storeTitle ?><br>
         </td>
     </tr>
@@ -45,63 +51,75 @@ use yii\helpers\Html;
         <th align="center">الوحدة</th>
         <th align="center">السعر الافرادي</th>
         <th align="center">السعر الاجمالي</th>
-        <?php if(!empty($model->total_price_discount_product)): ?>
+        <?php if (!empty($model->total_price_discount_product)): ?>
             <th align="center">الخصم</th>
             <th align="center">السعر بعد الخصم</th>
-        <?php endif;?>
+        <?php endif; ?>
 
     </tr>
-   <?php foreach ($products as $product) : ?>
+    <?php foreach ($products as $product) : ?>
         <tr>
-            <td align="center"><?= $product->id;?></td>
-            <td align="center"><?= $product->product->title ?? '';?></td>
-            <td align="center"><?= $product->count;?></td>
-            <td align="center"><?= $product->product->getCountTypeName('count_type');?></td>
-            <td align="center"><?= $product->amount;?></td>
-            <td align="center"><?= $product->total_product_amount;?></td>
-            <?php if(!empty($model->total_price_discount_product)): ?>
-                <td align="center"><?= $product->discount;?></td>
-                <td align="center"><?= $product->total_product_amount - $product->discount;?></td>
-            <?php endif;?>
+            <td align="center"><?= $product->id; ?></td>
+            <td align="center"><?= $product->product->title ?? ''; ?></td>
+            <td align="center"><?= $product->count; ?></td>
+            <td align="center"><?= $product->product->getCountTypeName('count_type'); ?></td>
+            <td align="center"><?= $product->amount; ?></td>
+            <td align="center"><?= $product->total_product_amount; ?></td>
+            <?php if (!empty($model->total_price_discount_product)): ?>
+                <td align="center"><?= $product->discount; ?></td>
+                <td align="center"><?= $product->total_product_amount - $product->discount; ?></td>
+            <?php endif; ?>
 
 
         </tr>
-    <?php endforeach;?>
-    <?php if(!empty($model->total_price_discount_product)): ?>
-        <tr bgcolor="#eee">
-            <td colspan="3" align="right"><b> مجموع الخصم</b></td>
-            <td align="center"><b><?=$model->total_price_discount_product ?></b></td>
-        </tr>
-    <?php endif;?>
-    <?php if(!empty($model->debt)): ?>
-        <tr bgcolor="#eee">
-            <td colspan="3" align="right"><b> الدين</b></td>
-            <td align="center"><b><?=$model->debt ?></b></td>
-        </tr>
-    <?php endif;?>
-    <?php if(!empty($model->total_count)): ?>
-        <tr bgcolor="#eee">
-            <td colspan="3" align="right"><b> العدد الاجمالي</b></td>
-            <td align="center"><b><?=$model->total_count ?></b></td>
-        </tr>
-    <?php endif;?>
-    <?php if(!empty($model->debt)): ?>
-        <tr bgcolor="#eee">
-            <td colspan="3" align="right"><b> القيمة الدين</b></td>
-            <td align="center"><b><?=$model->debt ?></b></td>
-        </tr>
-        <tr bgcolor="#eee">
-            <td colspan="3" align="right"><b> القمة المطلوبة</b></td>
-            <td align="center"><b><?=$model->debt+$model->total_amount ?></b></td>
-        </tr>
-    <?php endif;?>
-    <?php if(!empty($model->total_amount)): ?>
-        <tr bgcolor="#eee">
-            <td colspan="3" align="right"><b> القيمة المدفوعة</b></td>
-            <td align="center"><b><?=$model->total_amount ?></b></td>
-        </tr>
-    <?php endif;?>
+
+    <?php endforeach; ?>
+    <tr bgcolor="#eee">
+        <td colspan="2" align="right"><b> العدد الاجمالي</b></td>
+        <td align="center"><b><?= $model->total_count ?></b></td>
 
 
+    </tr>
+
+    <?php if (!empty($model->total_price_discount_product)): ?>
+    <tr bgcolor="#eee">
+        <td colspan="5"></td>
+        <td colspan="2" align="center"><b> القمة المطلوبة</b></td>
+        <td align="center"><b><?= $model->debt + $model->total_amount ?></b></td>
+    </tr>
+    <?php endif; ?>
+
+    <?php if (!empty($model->total_price_discount_product)): ?>
+        <tr bgcolor="#eee">
+            <td colspan="5"></td>
+            <td colspan="2" align="center"><b> مجموع الخصم</b></td>
+            <td align="center"><b><?= $model->total_price_discount_product ?></b></td>
+        </tr>
+    <?php endif; ?>
+    <?php if (!empty($model->debt)): ?>
+        <tr bgcolor="#eee">
+            <td colspan="5"></td>
+            <td colspan="2" align="center"><b> الدين</b></td>
+            <td align="center"><b><?= $model->debt ?></b></td>
+        </tr>
+    <?php endif; ?>
+
+    <?php if (!empty($model->debt)): ?>
+        <tr bgcolor="#eee">
+            <td colspan="5"></td>
+            <td colspan="2" align="center"><b> القيمة الدين</b></td>
+            <td align="center"><b><?= $model->debt ?></b></td>
+        </tr>
+
+
+
+    <?php endif; ?>
+
+    <tr bgcolor="#eee">
+        <td colspan="3"></td>
+        <td colspan="<?=(!empty($model->total_price_discount_product))?2:1?>" align="center"><b><?=$Arabic->money2str($model->total_amount, 'KWD', 'ar')  ?></b></td>
+        <td colspan="<?=(!empty($model->total_price_discount_product))?2:1?>" align="center"><b> السعر النهائي</b></td>
+        <td colspan="1" align="center"><b><?=$model->total_amount ?></b></td>
+    </tr>
 
 </table>
