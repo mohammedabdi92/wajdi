5e677dfda1eabd2205ed81822dc9a3142f7fc113
mohammed_alabadi <m.almahameed@syarah.com>
2022-10-15 14:29:47 +0300

    master

--- dashboard/views/product/index.php
+++ dashboard/views/product/index.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
-use kartik\grid\GridView;
+use yii\grid\GridView;
 
 
 /* @var $this yii\web\View */
@@ -35,14 +35,10 @@ $this->params['breadcrumbs'][] = $this->title;
         'pager' => [
             'options'=>['class'=>'pagination'],   // set clas name used in ui list of pagination
             'prevPageLabel' => 'Previous',   // Set the label for the "previous" page button
-            'nextPageLabel' => 'Next',   // Set the label for the "next" page button
-            'firstPageLabel'=>'First',   // Set the label for the "first" page button
-            'lastPageLabel'=>'Last',    // Set the label for the "last" page button
-            'nextPageCssClass'=>'next',    // Set CSS class for the "next" page button
-            'prevPageCssClass'=>'prev',    // Set CSS class for the "previous" page button
-            'firstPageCssClass'=>'first',    // Set CSS class for the "first" page button
-            'lastPageCssClass'=>'last',    // Set CSS class for the "last" page button
-            'maxButtonCount'=>10,    // Set maximum number of page buttons that can be displayed
+            'nextPageLabel' => '>',   // Set the label for the "next" page button
+            'firstPageLabel'=>'>>',   // Set the label for the "first" page button
+            'lastPageLabel'=>'<<',    // Set the label for the "last" page button
+            'maxButtonCount'=>20,    // Set maximum number of page buttons that can be displayed
         ],
         'columns' => [
 
