74ed8b306820625753a45fe9d83ba98808897e9d
mohammed_alabadi <m.almahameed@syarah.com>
2023-02-25 14:56:23 +0300

    master

--- dashboard/views/damaged/index.php
+++ dashboard/views/damaged/index.php
@@ -29,8 +29,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
             'id',
 
             'order_id',
--- dashboard/views/financial-withdrawal/index.php
+++ dashboard/views/financial-withdrawal/index.php
@@ -26,7 +26,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
             'id',
             [
