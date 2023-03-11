9d843f052db436c53caf23658d9b60b04123c9dc
mohammed_alabadi <m.almahameed@syarah.com>
2023-03-11 19:13:58 +0300

    master

--- dashboard/controllers/ReportsController.php
+++ dashboard/controllers/ReportsController.php
@@ -244,4 +244,119 @@ class ReportsController extends Controller
         ]);
     }
 
+
+    public function actionUserCashBox()
+    {
+        $modelSearch = new cashBoxSearch();
+        $modelSearch->load($this->request->queryParams);
+
+        $productQuery = Order::find();
+        $productQuery->joinWith('products.product');
+
+        // + orders (damaged)
+        $order_q =  Order::find()->select("total_amount");
+        $entries_q = Entries::find()->select("amount");
+        $damaged_q =  Damaged::find()->select('amount')->joinWith('order')->where(['status'=>Damaged::STATUS_RETURNED]);
+
+
+        // - inventory-order(dep) , outlay,returns,damaged
+        $inventory_order_q =  InventoryOrder::find()->select('total_cost');
+        $returns_q =  Returns::find()->select('amount')->joinWith('order');
+        $outlay_q =  Outlay::find()->select('amount');
+        $damaged_q_m =  Damaged::find()->select('amount')->joinWith('order')->where(['status'=>Damaged::STATUS_INACTIVE]);
+        $financial_withdrawal_q =  FinancialWithdrawal::find()->select('amount')->where(['status'=>FinancialWithdrawal::STATUS_NOT_PAYED]);
+
+        if($modelSearch->date_from)
+        {
+            $productQuery->andWhere(['>=', 'order.created_at', strtotime( $modelSearch->date_from)]);
+            $order_q->andWhere(['>=', 'created_at', strtotime( $modelSearch->date_from)]);
+            $entries_q->andWhere(['>=', 'put_date', $modelSearch->date_from]);
+            $damaged_q->andWhere(['>=', 'damaged.updated_at', strtotime( $modelSearch->date_from)]);
+            $returns_q->andWhere(['>=', 'returns.created_at', strtotime( $modelSearch->date_from)]);
+            $inventory_order_q->andWhere(['>=', 'created_at', strtotime( $modelSearch->date_from)]);
+            $outlay_q->andWhere(['>=', 'pull_date',  $modelSearch->date_from]);
+            $damaged_q_m->andWhere(['>=', 'damaged.updated_at', strtotime( $modelSearch->date_from)]);
+            $financial_withdrawal_q->andWhere(['>=', 'pull_date',  $modelSearch->date_from]);
+        }
+
+        if($modelSearch->date_to)
+        {
+
+            $modelSearch->date_to .= " 23:59:59";
+
+            $productQuery->andWhere(['<=', 'order.created_at', strtotime( $modelSearch->date_to)]);
+            $order_q->andWhere(['<=', 'created_at', strtotime( $modelSearch->date_to)]);
+            $entries_q->andWhere(['<=', 'put_date', $modelSearch->date_to]);
+            $damaged_q->andWhere(['<=', 'damaged.updated_at', strtotime( $modelSearch->date_to)]);
+            $returns_q->andWhere(['<=', 'returns.created_at', strtotime( $modelSearch->date_to)]);
+            $inventory_order_q->andWhere(['<=', 'created_at', strtotime( $modelSearch->date_to)]);
+            $outlay_q->andWhere(['<=', 'pull_date',  $modelSearch->date_to]);
+            $damaged_q_m->andWhere(['<=', 'damaged.updated_at', strtotime( $modelSearch->date_to)]);
+            $financial_withdrawal_q->andWhere(['<=', 'pull_date',  $modelSearch->date_to]);
+        }
+        if($modelSearch->store_id)
+        {
+            $productQuery->andWhere(['order.store_id'=>$modelSearch->store_id]);
+            $order_q->andWhere(['store_id'=>$modelSearch->store_id]);
+            $entries_q->andWhere(['store_id'=>$modelSearch->store_id]);
+            $returns_q->andWhere(['order.store_id'=>$modelSearch->store_id]);
+            $damaged_q->andWhere(['order.store_id'=>$modelSearch->store_id]);
+            $inventory_order_q->andWhere(['store_id'=>$modelSearch->store_id]);
+            $outlay_q->andWhere(['store_id'=>$modelSearch->store_id]);
+            $damaged_q_m->andWhere(['order.store_id'=>$modelSearch->store_id]);
+            $financial_withdrawal_q->andWhere(['store_id'=>$modelSearch->store_id]);
+        }
+
+
+
+        $damaged_mince = $damaged_q_m->sum('amount');
+        $outlay_mince = $outlay_q->sum('amount');
+        $inventory_order_mince = $inventory_order_q->sum('total_cost');
+        $damaged_plus = $damaged_q->sum('amount');
+        $returns_mince = $returns_q->sum('amount');
+        $entries_pluse =  $entries_q->sum('amount');
+        $order_pluse =  $order_q->sum('total_amount');
+        $order_q->andWhere('order.debt is  null');
+        $orderw_pluse =  $order_q->sum('total_amount');
+
+        $financial_withdrawal_mince = $financial_withdrawal_q->sum('amount');
+
+        $total_returns_amount = $productQuery->sum('(select sum(returns.amount) from returns where returns.order_id = order.id and  order_product.product_id = returns.product_id)')  ;
+        $total_dept_returns_amount = $productQuery->sum('(select sum(returns.count * product.price) from returns where returns.order_id = order.id and  order_product.product_id = returns.product_id)')  ;
+        $total_profit_returns_amount  =  $total_returns_amount  - $total_dept_returns_amount ;
+
+        $productQuery->andWhere('order.debt is  null');
+        $total_dept =  round($productQuery->sum('(product.price * order_product.count) '),2);
+
+
+        $box_in = (double)$order_pluse + (double)$entries_pluse + (double)$damaged_plus;
+        $box_out =   (double)$inventory_order_mince + (double)$outlay_mince + (double)$damaged_mince + (double)$financial_withdrawal_mince+(double)$returns_mince;
+
+
+        $cash_amount =  $box_in - $box_out;
+        $cash_amount = round($cash_amount, 2);
+        $cash_amount_without_inventory_order = round( $cash_amount+$inventory_order_mince, 2);
+
+        $total_profit  =  $orderw_pluse -  $total_dept - $total_profit_returns_amount ;
+        $total_profit_without_damaged_outlay =  $total_profit -$damaged_mince -$outlay_mince;
+
+        return $this->render('user-cash-box', [
+            'modelSearch'=>$modelSearch,
+            'cash_amount'=>$cash_amount,
+            'total_profit'=>round($total_profit,2),
+            'total_profit_without_damaged_outlay'=>round($total_profit_without_damaged_outlay,2),
+            'cash_amount_without_inventory_order'=>$cash_amount_without_inventory_order,
+            'box_in'=> round($box_in, 2),
+            'box_out'=> round($box_out, 2),
+            'order_pluse'=>round($order_pluse, 2),
+            'entries_pluse'=> round($entries_pluse, 2),
+            'returns_mince'=> round($returns_mince, 2),
+            'damaged_plus'=> round($damaged_plus, 2),
+            'inventory_order_mince'=>round($inventory_order_mince, 2),
+            'outlay_mince'=> round($outlay_mince, 2),
+            'financial_withdrawal_mince'=> round($financial_withdrawal_mince, 2),
+            'damaged_mince'=> round($damaged_mince, 2),
+        ]);
+    }
+
 }
---
+++ dashboard/views/reports/user-cash-box.php
@@ -0,0 +1,99 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\InventorySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'مواد الافرع');
+$this->params['breadcrumbs'][] = $this->title;
+
+
+
+?>
+<div class="inventory-index row">
+    <?php $form = \yii\widgets\ActiveForm::begin([
+        'method' => 'get',
+    ]); ?>
+    <?= $form->field($modelSearch, 'store_id')->dropDownList( [''=>'اختر ....'] + \yii\helpers\ArrayHelper::map(\common\models\Store::find()->where(['status'=>1])->all(), 'id', 'name'))->label("المحل"); ?>
+    <label> التاريخ</label>
+    <?= \kartik\daterange\DateRangePicker::widget([
+        'model' => $modelSearch,
+        'attribute' => 'date',
+        'language' => 'en',
+        'convertFormat' => true,
+        'startAttribute' => 'date_from',
+        'endAttribute' => 'date_to',
+        'pluginOptions' => [
+            'timePicker' => true,
+            'timePickerIncrement' => 30,
+            'locale' => [
+                'applyLabel' => 'تطبيق',
+                'cancelLabel' => 'الغاء',
+                'format' => 'Y-m-d'
+            ]
+        ]
+    ]); ?>
+    <br>
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'بحث'), ['class' => 'btn btn-primary']) ?>
+    </div>
+    <?php \yii\widgets\ActiveForm::end(); ?>
+
+
+    <div class="col-xs-12 col-md-6">
+        <p class="lead">الصندوق</p>
+        <div class="table-responsive">
+            <table class="table">
+                <tbody>
+                <tr>
+                    <th style="width:50%">صافي الصندوق:</th>
+                    <td><?= $cash_amount?></td>
+                </tr>
+
+                <tr>
+                    <th style="width:50%">المبيعات :</th>
+                    <td><?= $order_pluse?></td>
+                </tr>
+
+                <tr>
+                    <th style="width:50%">التوالف المرجعة :</th>
+                    <td><?= $damaged_plus?></td>
+                </tr>
+
+                <tr>
+                    <th style="width:50%">المشتريات :</th>
+                    <td><?= $inventory_order_mince?></td>
+                </tr>
+                <tr>
+                    <th style="width:50%">المصروفات :</th>
+                    <td><?= $outlay_mince?></td>
+                </tr>
+                <tr>
+                    <th style="width:50%">المسحوبات :</th>
+                    <td><?= $financial_withdrawal_mince?></td>
+                </tr>
+                <tr>
+                    <th style="width:50%">التوالف الغير مرجعة :</th>
+                    <td><?= $damaged_mince?></td>
+                </tr>
+                <tr>
+                    <th style="width:50%"> المرجعة :</th>
+                    <td><?= $returns_mince?></td>
+                </tr>
+                <tr>
+                    <th style="width:50%">الربح الصافي:</th>
+                    <td><?= $total_profit_without_damaged_outlay?></td>
+                </tr>
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
